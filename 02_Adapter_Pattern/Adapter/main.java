b8e3a1b4929960bafff8460c27162b074f4e0d0e
Jiun Park <zieunpk0924@naver.com>
2023-03-09 22:08:30 +0900

    Adapter_Math

---
+++ 02_Adapter_Pattern/Adapter/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Adapter adapter = new AdapterImpl();
+        System.out.println(adapter.twiceOf(100f));
+        System.out.println(adapter.halfOf(50f));
+    }
+}
